a4b3c1bc92694b1ce093e20a47fbb50933ee41c7
Ethan Howell <ethanhowell@fb.com>
2020-06-23 16:48:06 -0500

    created adapter class for recycler view

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.simpletodo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+    Button btnAdd;
+    EditText etTodo;
+    RecyclerView rvList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.buttonAdd);
+        etTodo = findViewById(R.id.etAdd);
+        rvList = findViewById(R.id.rvList);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Buy cheese");
+        items.add("Drink milk");
     }
 }
