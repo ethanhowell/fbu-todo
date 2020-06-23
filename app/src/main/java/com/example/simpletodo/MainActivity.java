05c3e8558e4572c4936404929232a35c0dc56cc8
Ethan Howell <ethanhowell@fb.com>
2020-06-23 16:58:04 -0500

    got adding new items working correctly

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.simpletodo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -32,8 +34,18 @@ public class MainActivity extends AppCompatActivity {
         items.add("Buy cheese");
         items.add("Drink milk");
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         rvList.setAdapter(itemsAdapter);
         rvList.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                items.add(etTodo.getText().toString());
+                itemsAdapter.notifyItemChanged(items.size() - 1);
+                etTodo.setText("");
+                Toast.makeText(getApplicationContext(), "Item successfully added", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
