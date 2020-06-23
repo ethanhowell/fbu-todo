a855416156e87f8be5b79d15e748ccaf43d4836b
Ethan Howell <ethanhowell@fb.com>
2020-06-23 17:37:49 -0500

    implemented persistence

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.simpletodo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,10 +11,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = getClass().getCanonicalName();
 
     List<String> items;
     Button btnAdd;
@@ -30,15 +38,13 @@ public class MainActivity extends AppCompatActivity {
         etTodo = findViewById(R.id.etAdd);
         rvList = findViewById(R.id.rvList);
 
-        items = new ArrayList<>();
-        items.add("Buy milk");
-        items.add("Buy cheese");
-        items.add("Drink milk");
+        loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
             @Override
             public void onItemLongClick(int position) {
                 items.remove(position);
+                writeItems();
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Item successfully removed", Toast.LENGTH_SHORT).show();
             }
@@ -51,11 +57,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 items.add(etTodo.getText().toString());
+                writeItems();
                 itemsAdapter.notifyItemChanged(items.size() - 1);
                 etTodo.setText("");
                 Toast.makeText(getApplicationContext(), "Item successfully added", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e(TAG, "writeItems: error writing to file", e);
+        }
+    }
+
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e(TAG, "error loading items from file", e);
+            items = new ArrayList<>();
+        }
     }
 
 }
